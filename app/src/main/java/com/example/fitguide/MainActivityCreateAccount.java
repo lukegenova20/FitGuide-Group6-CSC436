6557f3aa081a22e6e072b750c2c2469c07e45bd3
epicananmay2 <58270555+epicananmay2@users.noreply.github.com>
2023-10-23 21:30:41 -0700

    Validation Checks! (Third Commit)
    Updated Login and Create Account Page.

--- app/src/main/java/com/example/fitguide/MainActivityCreateAccount.java
+++ app/src/main/java/com/example/fitguide/MainActivityCreateAccount.java
@@ -1,17 +1,25 @@
 package com.example.fitguide;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -22,6 +30,7 @@ public class MainActivityCreateAccount extends AppCompatActivity {
     Button create_act ;
     TextInputEditText f_name,l_name,weight,username,password;
     FirebaseAuth firebaseAuth;
+    TextInputLayout parent_password;
     FirebaseFirestore firebaseFirestore;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -32,39 +41,89 @@ public class MainActivityCreateAccount extends AppCompatActivity {
         l_name = findViewById(R.id.last_name);
         weight = findViewById(R.id.weight);
         username = findViewById(R.id.user_name);
+        parent_password  = findViewById(R.id.parent_password);
         password = findViewById(R.id.password);
         create_act = findViewById(R.id.create_btn_2);
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivityCreateAccount.this);
+        builder.setIcon(R.drawable.baseline_error_24);
+        builder.setTitle(" ");
+        builder.setCancelable(true);
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                parent_password.setHelperText("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         create_act.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("arun","Sds");
+                if(f_name.getText().toString().isEmpty()){
+                    f_name.setError("*Fill");
+                }else{
+                    if(l_name.getText().toString().isEmpty()){
+                        l_name.setError("*Fill");
+                    }else{
+                        if(weight.getText().toString().isEmpty()){
+                            weight.setError("*Fill");
+                        }else{
+                            if(username.getText().toString().isEmpty()){
+                                username.setError("*Fill");
+                            }else{
+                                if(password.getText().toString().isEmpty()){
+                                    parent_password.setHelperText("password should not be empty");
+                                }else{
+                                    if(password.getText().toString().length()>5 && password.getText().toString().length()<=16){
+                                        firebaseAuth.createUserWithEmailAndPassword(
+                                                username.getText().toString(),password.getText().toString()
+                                        ).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                            @Override
+                                            public void onSuccess(AuthResult authResult) {
+                                                Map<String, Object> datas = new HashMap<>();
+                                                datas.put("First Name",f_name.getText().toString());
+                                                datas.put("Last Name",l_name.getText().toString());
+                                                datas.put("Weight(LBS)",weight.getText().toString());
+                                                firebaseFirestore.collection(authResult.getUser().getUid()).
+                                                        document("Personal_info").set(datas).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                            @Override
+                                                            public void onSuccess(Void unused) {
+                                                                startActivity(new Intent(MainActivityCreateAccount.this,MainActivity2.class));
+                                                            }
+                                                        });
 
-                firebaseAuth.createUserWithEmailAndPassword(
-                        username.getText().toString(),password.getText().toString()
-                ).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        Map<String, Object> datas = new HashMap<>();
-                        datas.put("First Name",f_name.getText().toString());
-                        datas.put("Last Name",l_name.getText().toString());
-                        datas.put("Weight(LBS)",weight.getText().toString());
-                        firebaseFirestore.collection(authResult.getUser().getUid()).
-                                document("Personal_info").set(datas).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void unused) {
-                                        Toast.makeText(MainActivityCreateAccount.this,"Successfully created",Toast.LENGTH_LONG).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                builder.setMessage(e.getMessage());
+                                                AlertDialog alertDialog = builder.create();
+                                                alertDialog.show();
+                                            }
+                                        });
+                                    }else if(password.getText().toString().length()<=5){
+                                        parent_password.setHelperText("password must be greater than 5");
+                                    }else{
+                                        parent_password.setHelperText("password must be lesser than 17");
                                     }
-                                });
-
+                                }
+                            }
+                        }
                     }
-                });
+                }
 
             }
         });
     }
 }
-
-
 
--- app/src/main/java/com/example/fitguide/MainActivityLogin.java
+++ app/src/main/java/com/example/fitguide/MainActivityLogin.java
@@ -1,15 +1,20 @@
 package com.example.fitguide;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -25,7 +30,7 @@ import java.util.Map;
 public class MainActivityLogin extends AppCompatActivity {
     Button create_act,sign_button;
     TextInputEditText username,password;
-    TextInputLayout parent_password;
+    TextInputLayout parent_password,parent_username;
     FirebaseAuth firebaseAuth ;
 
 
@@ -36,26 +41,55 @@ public class MainActivityLogin extends AppCompatActivity {
         setContentView(R.layout.activity_main_login);
         create_act = findViewById(R.id.create_act_btn);
         parent_password = findViewById(R.id.parent_password_sign);
+        parent_username = findViewById(R.id.parent_username_sign);
         sign_button =  findViewById(R.id.login_btn);
         username = findViewById(R.id.username_sign);
         password = findViewById(R.id.pass_sign);
         firebaseAuth = FirebaseAuth.getInstance();
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                parent_password.setHelperText("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         sign_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(username.getText().toString().isEmpty() || username.getText().toString().equals("")){
                     username.setError("*Fill");
+                    parent_password.setHelperText("");
                 }else{
-                    if(password.getText().toString().length()<6){
-                        parent_password.setHelperText("*must be greater than 5");
+                    if(password.getText().toString().isEmpty()){
+                        parent_password.setHelperText("*Fill");
                     }else{
                         firebaseAuth.signInWithEmailAndPassword(username.getText().toString(),password.getText().toString())
                                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                                     @Override
                                     public void onSuccess(AuthResult authResult) {
-                                        Toast.makeText(MainActivityLogin.this,"Successfully Login",Toast.LENGTH_LONG).show();
                                         startActivity(new Intent(MainActivityLogin.this,MainActivity2.class));
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivityLogin.this);
+                                        builder.setIcon(R.drawable.baseline_error_24);
+                                        builder.setTitle(" ");
+                                        builder.setCancelable(true);
+                                        builder.setMessage("Username/Password is Invalid");
+                                        AlertDialog alertDialog = builder.create();
+                                        alertDialog.show();
                                     }
                                 });
                     }
@@ -65,12 +99,14 @@ public class MainActivityLogin extends AppCompatActivity {
 
             }
         });
-        create_act.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivityLogin.this, MainActivityCreateAccount.class));
-            }
-        });
+        create_act.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivityLogin.this, MainActivityCreateAccount.class));
+                    }
+                }
+        );
 
 
     }
