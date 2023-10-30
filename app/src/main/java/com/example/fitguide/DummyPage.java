1a0b6b6b359da5dac7b726f44d2af104dd89ece2
lukegenova20 <lukegenova24@gmail.com>
2023-10-29 18:03:38 -0700

    Created a workout routine style selection page

---
+++ app/src/main/java/com/example/fitguide/DummyPage.java
@@ -0,0 +1,14 @@
+package com.example.fitguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DummyPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_page);
+    }
+}
