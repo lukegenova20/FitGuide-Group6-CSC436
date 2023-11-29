c95d3721883ccd7b732cc820aa35487c7b287018
alexrendlerCS <126621758+alexrendlerCS@users.noreply.github.com>
2023-11-28 21:45:53 -0700

    High/Low Calorie Options Added

--- app/src/main/java/com/example/fitguide/Diet_Creation.java
+++ app/src/main/java/com/example/fitguide/Diet_Creation.java
@@ -4,18 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Diet_Creation extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_diet);
-
         Button backButton = findViewById(R.id.back_button);
-
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +26,90 @@ public class Diet_Creation extends AppCompatActivity {
             }
         });
 
+        Button sundayButton;
+        Button mondayButton;
+        Button tuesdayButton;
+        Button wednesdayButton;
+        Button thursdayButton;
+        Button fridayButton;
+        Button saturdayButton;
 
+        // Initialize views
+        sundayButton = findViewById(R.id.sunday_button);
+        mondayButton = findViewById(R.id.monday_button);
+        tuesdayButton = findViewById(R.id.tuesday_button);
+        wednesdayButton = findViewById(R.id.wednesday_button);
+        thursdayButton = findViewById(R.id.thursday_button);
+        fridayButton = findViewById(R.id.friday_button);
+        saturdayButton = findViewById(R.id.saturday_button);
+
+        // Set click listener for Sunday button
+        sundayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view, R.id.sunday_workout);
+            }
+        });
+
+        mondayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view, R.id.monday_workout);
+            }
+        });
+
+        tuesdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view, R.id.tuesday_workout);
+            }
+        });
+
+        wednesdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view, R.id.wednesday_workout);
+            }
+        });
+
+        thursdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view, R.id.thursday_workout);
+            }
+        });
+        fridayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view, R.id.friday_workout);
+            }
+        });
+
+        saturdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(view, R.id.saturday_workout);
+            }
+        });
+    }
+
+    // Method to show the popup menu
+    private void showPopupMenu(View view, int workoutTextViewId) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.getMenuInflater().inflate(R.menu.menu_calorie_option, popupMenu.getMenu());
+        TextView workoutTextView = findViewById(workoutTextViewId);
+
+        // Set item click listener for the popup menu
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                // Update the text based on the selected option
+                workoutTextView.setText(menuItem.getTitle());
+                return true;
+            }
+        });
+
+        // Show the popup menu
+        popupMenu.show();
     }
 }
